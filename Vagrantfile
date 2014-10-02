ca744ca8d80cd9014a73eed23d25a5b26bf1cc21
Ben Sandberg <ben@pdxfixit.com>
2014-10-02 09:59:50 -0700

    replaced Puppet chained events with dependencies, improved Vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,9 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
 $script = <<SCRIPT
 mkdir -p /etc/puppet/modules;
 if [ ! -d /etc/puppet/modules/file_concat ]; then
@@ -7,6 +11,9 @@ puppet module install ispavailability/file_concat
 fi
 if [ ! -d /etc/puppet/modules/apt ]; then
 puppet module install puppetlabs-apt
+fi
+if [ ! -d /etc/puppet/modules/java ]; then
+puppet module install puppetlabs-java
 fi
 if [ ! -d /etc/puppet/modules/elasticsearch ]; then
 puppet module install elasticsearch-elasticsearch
@@ -16,21 +23,21 @@ puppet module install elasticsearch-logstash
 fi
 SCRIPT
 
-Vagrant.configure("2") do |config|
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  # set to false, if you do NOT want to check the correct VirtualBox Guest Additions version when booting this box
+  if defined?(VagrantVbguest::Middleware)
+    config.vbguest.auto_update = true
+  end
+
+  config.vm.box = "hashicorp/precise64"
+  config.vm.network :forwarded_port, guest: 5601, host: 5601
   config.vm.network :forwarded_port, guest: 9200, host: 9200
   config.vm.network :forwarded_port, guest: 9300, host: 9300
-  config.vm.network :forwarded_port, guest: 5601, host: 5601
-  config.vm.provision :shell do |shell|
-    shell.inline = $script
+  config.vm.provider :virtualbox do |vb|
+    vb.customize ["modifyvm", :id, "--cpus", "2", "--memory", "2048"]
   end
-  config.vm.provision :puppet do |puppet|
-    puppet.manifests_path = "manifests"
-    puppet.manifest_file  = "default.pp"
-  end
-  config.vm.provider "virtualbox" do |v|
-    v.customize ["modifyvm", :id, "--cpus", "2"]
-    v.customize ["modifyvm", :id, "--memory", "2048"]
-  end
+  config.vm.provision "shell", inline: $script
+  config.vm.provision "puppet", manifests_path: "manifests", manifest_file: "default.pp"
+
 end
--- manifests/default.pp
+++ manifests/default.pp
@@ -6,88 +6,115 @@ define append_if_no_such_line($file, $line, $refreshonly = 'false') {
    }
 }
 
+# Add the repositories to APT
+class { 'apt':
+  always_apt_update => true,
+}->
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update -qq'
+}
 
-class el {
-  include apt
-  include stdlib
+apt_key { 'elasticsearch':
+  ensure => 'present',
+  id     => 'D88E42B4',
+  source => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+}
 
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
-  } ->
-  file { '/vagrant/elasticsearch':
-    ensure => directory,
-    owner => 'vagrant',
-    group => 'vagrant',
-  } ->
-  class { 'elasticsearch':
-    java_install => true,
-    package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.1.deb',
-    config => {
-      'cluster' => { 
-        'name' => 'vagrant_elasticsearch'
-      },
-      'index' => {
-        'number_of_replicas' => '0',
-        'number_of_shards' => '1',
-      },
-      'network' => {
-        'host' => '0.0.0.0',
-      }
+apt::source { 'elasticsearch':
+  comment     => 'Elasticsearch 1.3 repository',
+  location    => 'http://packages.elasticsearch.org/elasticsearch/1.3/debian/',
+  include_src => false,
+  release     => 'stable',
+  repos       => 'main',
+}
+
+apt::source { 'logstash':
+  comment     => 'Logstash 1.4 repository',
+  location    => 'http://packages.elasticsearch.org/logstash/1.4/debian',
+  include_src => false,
+  release     => 'stable',
+  repos       => 'main',
+}
+
+file { '/vagrant/elasticsearch':
+  ensure => 'directory',
+  group  => 'vagrant',
+  owner  => 'vagrant',
+}
+
+# Java is required
+class { 'java': }
+
+# Elasticsearch
+class { 'elasticsearch':
+  autoupgrade => true,
+  config      => {
+    'cluster' => {
+      'name'  => 'vagrant_elasticsearch'
+    },
+    'index'   => {
+      'number_of_replicas' => '0',
+      'number_of_shards'   => '1',
+    },
+    'network' => {
+      'host'  => '0.0.0.0',
     }
-  } ->
-  service { "elasticsearch-service":
-    name => 'elasticsearch',
-    ensure => running
-  } ->
-  exec { 'add logstash repo key':
-    command => '/usr/bin/wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | /usr/bin/apt-key add -'
-  } ->
-  exec { 'add logstash repo':
-    command => '/bin/echo "deb http://packages.elasticsearch.org/logstash/1.4/debian stable main" >> /etc/apt/sources.list'
-  } ->
-  exec { 'apt-get update 1':
-    command => '/usr/bin/apt-get update'
-  } ->
-  class { 'logstash':
-  } ->
-  file { "/etc/logstash/conf.d/logstash":
-    ensure => "/vagrant/confs/logstash/logstash.conf",
-  }
+  },
+  ensure      => 'present',
+  require     => [ Class['apt'], Class['java'], File['/vagrant/elasticsearch'] ],
 }
 
-class kibana {
-  include apt
-  include stdlib
-  
-  package { "nginx":
-    ensure => installed
-  } ->
-  file { "/etc/nginx/sites-available/default":
-    ensure => link,
-    target => "/vagrant/confs/nginx/default"
-  } ->
-  service { "nginx-ruuing":
-    name => 'nginx',
-    ensure => running
-  } ->
-  exec { 'reload nginx':
-    command => '/etc/init.d/nginx reload'
-  }
-  
-  package { "curl":
-    ensure => installed
-  } ->
-  file { '/vagrant/kibana':
-    ensure => directory,
-    owner => 'vagrant',
-    group => 'vagrant',
-  } ->
-  exec { 'download kibana':
-    command => '/usr/bin/curl https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz | /bin/tar xvz -C /vagrant/kibana'
-  }
+service { 'elasticsearch-service':
+  ensure  => 'running',
+  name    => 'elasticsearch',
+  require => [ Class['elasticsearch'] ],
 }
 
-include el
-include kibana
+# Logstash
+class { 'logstash':
+  autoupgrade => true,
+  ensure      => 'present',
+  require     => [ Class['elasticsearch'] ],
+}
 
-Class['el'] -> Class['kibana']
+file { '/etc/logstash/conf.d/logstash':
+  ensure  => '/vagrant/confs/logstash/logstash.conf',
+  require => [ Class['logstash'] ],
+}
+
+package { 'nginx':
+  ensure  => 'present',
+  require => [ Class['apt'] ],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'link',
+  require => [ Package['nginx'] ],
+  target  => '/vagrant/confs/nginx/default',
+}
+
+service { "nginx-service":
+  ensure  => 'running',
+  name    => 'nginx',
+  require => [ Package['nginx'] ],
+}->
+exec { 'reload nginx':
+  command => '/etc/init.d/nginx reload',
+}
+
+# Kibana
+package { 'curl':
+  ensure  => 'present',
+  require => [ Class['apt'] ],
+}
+
+file { '/vagrant/kibana':
+  ensure => 'directory',
+  group  => 'vagrant',
+  owner  => 'vagrant',
+}
+
+exec { 'download_kibana':
+  command => '/usr/bin/curl https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz | /bin/tar xz -C /vagrant/kibana',
+  require => [ Package['curl'], File['/vagrant/kibana'] ],
+}
